cf0df73d3031c033ad3cf7e06b910ba3479b945c
Keshav <you@example.com>
2018-07-01 22:36:46 +0530

    ffftf

--- src/Java/SearchNo.java
+++ src/Java/SearchNo.java
@@ -1,20 +1,34 @@
 package Java;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchNo {
 	
 	public static void main(String[] args) {
 		
 		int input[]={1,2,3,4,5,6};
 		int sum=0,median=0;
-		for(int i=0;i<=input.length;i++)
+//		Set set=new HashSet();
+		List set=new ArrayList();
+		for(int a:input)
 		{
-			sum=sum+input[i];
+//			sum=sum+input[i];
 			
+			set.add(a);
 		}
-		if(input.length%2!=0)
+		int num=3;
+		System.out.println(set.contains(num));
+		
+		
+		
+		
+		
+		/*if(input.length%2!=0)
 		median=(input.length+1)/2;
 		else if(input.length%2==0)
-		median=(input.length/2)+((input.length/2)+1);
+		median=(input.length/2)+((input.length/2)+1);*/
 		
 		
 		
